963f499605c8924231b82bd7ee5ae3c2b41ae728
Jialiang Gu <jgu1@andrew.cmu.edu>
2012-11-14 22:16:13 -0500

    poluted

--- TimeMachineBeta/db/schema.rb
+++ TimeMachineBeta/db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121115030433) do
+ActiveRecord::Schema.define(:version => 20121115031219) do
 
   create_table "auctions", :force => true do |t|
     t.string   "name"
     t.integer  "base_price"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.datetime "expired_at"
   end
 
 end
